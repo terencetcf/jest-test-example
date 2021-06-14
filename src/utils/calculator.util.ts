56ed173cec2a258209d5291c4c356bcefa184809
Terence Tai <terencetai@pulsifi.me>
2021-06-14 20:37:20 +0800

    add subtract method

--- src/utils/calculator.util.ts
+++ src/utils/calculator.util.ts
@@ -1,5 +1,8 @@
 const sum = (a: number, b: number) => a + b;
 
+const subtract = (a: number, b: number) => a - b;
+
 export const calculator = {
   sum,
+  subtract,
 };
--- tests/utils/calculator.util.test.ts
+++ tests/utils/calculator.util.test.ts
@@ -8,4 +8,12 @@ describe("calculator", () => {
       expect(actual).toEqual(3);
     });
   });
+
+  describe("subtract", () => {
+    it("should return the correct result of a - b", () => {
+      const actual = calculator.subtract(3, 2);
+
+      expect(actual).toEqual(1);
+    });
+  });
 });
