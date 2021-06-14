8ae4bc86c11480aadadccf22fc7a47a903799b91
Terence Tai <terencetai@pulsifi.me>
2021-06-14 20:18:46 +0800

    add test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -6,9 +6,14 @@ const config: Config.InitialOptions = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/",
   }),
+  testPathIgnorePatterns: ["archives"],
   transform: {
     "^.+\\.(t|j)s$": "ts-jest",
   },
+  coverageDirectory: "test-coverage",
+  // collectCoverage: false,
+  // collectCoverageFrom: ["./src/**/*.{ts,tsx}"],
+  // coveragePathIgnorePatterns: ["^.+\\.d\\.ts$"],
   verbose: true,
 };
 
