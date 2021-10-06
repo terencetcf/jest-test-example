fc0c41de1aa393f683c303b58b3f909b7d727c17
Terence Tai <terencetcf@hotmail.com>
2021-10-06 11:42:23 +0800

    testing

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { calculator } from '@utils';
+
+const a = calculator.division(100, 2);
+
+const b = calculator.division(a, 2);
+
+const c = calculator.division(b, 2);
+
+const d = calculator.division(c, 2);
+
+const e = calculator.division(d, 2);
